21bc5ffbbbaf2679f770231163b0638416d9ee4f
Neil <njtynan@live.com>
2025-05-15 23:03:51 +0100

    Removed remaining references to grant table from grantDAO.py and rest_server.py.

--- grantDAO.py
+++ grantDAO.py
@@ -2,62 +2,6 @@
 import mysql.connector
 import dbconfig as cfg
 
-#grantDAO()
-class grantDAO:
-    connection=""
-    cursor =''
-    host=       ''
-    user=       ''
-    password=   ''
-    database=   ''
-
-#config file
-    def __init__(self):
-        self.host=       cfg.mysql['host']
-        self.user=       cfg.mysql['user']
-        self.password=   cfg.mysql['password']
-        self.database=   cfg.mysql['database']
-
-    def getcursor(self): 
-        self.connection = mysql.connector.connect(
-            host=       self.host,
-            user=       self.user,
-            password=   self.password,
-            database=   self.database,
-        )
-        self.cursor = self.connection.cursor()
-        return self.cursor
-
-    def closeAll(self):
-        self.connection.close()
-        self.cursor.close()
-
-    def getAll(self):
-        cursor = self.getcursor()
-        sql="select * from funding"
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        returnArray = []
-        #print(results)
-        for result in results:
-            #print(result)
-            returnArray.append(self.convertToDictionary(result))
-        
-        self.closeAll()
-        return returnArray
-    
-    def convertToDictionary(self, resultLine):
-        attkeys=['id','title','author','year', 'institution', "programme", "amount"]
-        grant = {}
-        currentkey = 0
-        for attrib in resultLine:
-            grant[attkeys[currentkey]] = attrib
-            currentkey = currentkey + 1 
-        return grant
-
-        
-grantDAO = grantDAO()
-
 # researcherDAO
 class researcherDAO:
     connection=""
--- rest_server.py
+++ rest_server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template, abort
-from grantDAO import grantDAO, researcherDAO
+from grantDAO import researcherDAO
 
 app = Flask(__name__)
 
@@ -11,65 +11,8 @@ grants = []
 def home():
     return render_template('1table.html')
 
-# Table 1 CRUD
 
-@app.route('/grants/<int:id>', methods=['GET'])
-def find_by_id(id):
-    return jsonify(grantDAO.findByID(id))
-
-@app.route('/grants', methods=['GET'])
-def get_grants():
-    return jsonify(grantDAO.getAll())
-
-@app.route('/grants', methods=['POST'])
-def create_grant():
-    jsonstring = request.json
-    grant = {}
-    if "title" not in jsonstring:
-        abort(400)
-    grant["title"] = jsonstring["title"]
-    if "author" not in jsonstring:
-        abort(400)
-    grant["author"] = jsonstring["author"]
-    if "year" not in jsonstring:
-        abort(400)
-    grant["year"] = jsonstring["year"]
-    if "institution" not in jsonstring:
-        abort(400)
-    grant["institution"] = jsonstring["institution"]
-    if "programme" not in jsonstring:
-        abort(400)
-    grant["programme"] = jsonstring["programme"]
-    if "amount" not in jsonstring:
-        abort(400)
-    grant["amount"] = jsonstring["amount"]
-    return jsonify(grantDAO.create(grant))
-# abort option
-
-
-@app.route('/grants/<int:id>', methods=['PUT'])
-def update_grant(id):
-    jsonstring = request.json
-    grant = {}
-    if "title" in jsonstring:
-        grant["title"] = jsonstring["title"]
-    if "author" in jsonstring:
-        grant["author"] = jsonstring["author"]
-    if "year" in jsonstring:
-        grant["year"] = jsonstring["year"]
-    if "institution" in jsonstring:
-        grant["institution"] = jsonstring["institution"]
-    if "programme" in jsonstring:
-        grant["programme"] = jsonstring["programme"]
-    if "amount" in jsonstring:
-        grant["amount"] = jsonstring["amount"]
-    return jsonify(grantDAO.update(id, grant))
-
-@app.route('/grants/<int:id>', methods=['DELETE'])
-def delete_grant(id):
-    return jsonify(grantDAO.delete(id))
-
-# Table 2 CRUD
+# Researcher Table CRUD
 
 @app.route('/researcher/<int:id>', methods=['GET'])
 def find_by_idresearcher(id):
